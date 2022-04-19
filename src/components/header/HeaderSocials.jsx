23cc2cae6ffec13cdde6f977ad57e2f6da22de38
Haider Zaman <chhaider10954@gmail.com>
2022-04-20 01:39:04 +0500

    Header Completed

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import {FiLinkedin} from 'react-icons';
-import {BsGithub} from 'react-icons';
-import {FiDribbble} from 'react-icons';
+import {BsLinkedin} from 'react-icons/bs';
+import {FaGithub} from 'react-icons/fa';
+import {FiDribbble} from 'react-icons/fi';
 
 const HeaderSocials = () => {
 return (
     <div className="header__socials">
-        <a href="https://linkedin.com" target='_blank' rel="noreferrer"><FiLinkedin/></a> 
-        <a href="https://github.com" target='_blank' rel="noreferrer"><BsGithub /></a>
+        <a href="https://linkedin.com" target='_blank' rel="noreferrer"><BsLinkedin/></a> 
+        <a href="https://github.com" target='_blank' rel="noreferrer"><FaGithub /></a>
         <a href="https://dribble.com" target='_blank' rel="noreferrer"><FiDribbble /></a>
     </div>
     ) 
