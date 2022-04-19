668db5c54b57a9c886632ea41fc548c7673ce70b
Haider Zaman <chhaider10954@gmail.com>
2022-04-20 04:03:19 +0500

    Nav floating component completed

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import './nav.css'
 import {AiOutlineHome} from "react-icons/ai";
@@ -5,15 +6,62 @@ import {AiOutlineUser} from "react-icons/ai";
 import {BsBookHalf} from "react-icons/bs";
 import {AiOutlineCustomerService} from "react-icons/ai";
 import {AiOutlineMessage} from "react-icons/ai";
+import { useState } from "react";
 
 const Nav = () => {
+    const [activeNav, setActiveNav] = useState('#')
 return (
     <nav>
-        <a href="#home" className="active"><AiOutlineHome/></a>
-        <a href="#about"><AiOutlineUser/></a>
-        <a href="#experience"><BsBookHalf/></a>
-        <a href="#services"><AiOutlineCustomerService/></a>
-        <a href="#contact"><AiOutlineMessage/></a>
+
+        <a 
+        
+        href="#" 
+        onClick={() => setActiveNav('#')}  
+        className={activeNav === '#' ? 'active' : ' ' }>
+            <AiOutlineHome/>
+        
+        </a>
+
+        <a 
+        href="#about"
+        onClick={() => setActiveNav('#about')} 
+        className={activeNav === '#' ? 'active' : ' ' }
+        >
+            <AiOutlineUser/>
+            
+        </a>
+
+        <a 
+        
+        href="#experience"
+        onClick={() => setActiveNav('#experience')} 
+        className={activeNav === '#' ? 'active' : ' ' }
+        >
+            <BsBookHalf/>
+        
+        </a>
+
+        <a
+        
+        href="#services"
+        onClick={() => setActiveNav('#services')} 
+        className={activeNav === '#' ? 'active' : ' ' }
+        
+        >
+            <AiOutlineCustomerService/>
+        
+        </a>
+
+        <a 
+        
+        href="#contact"
+        onClick={() => setActiveNav('#contact')} 
+        className={activeNav === '#' ? 'active' : ' ' }
+
+        >
+            <AiOutlineMessage/>
+            
+        </a>
 
     </nav>
 
