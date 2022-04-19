4cce9f06a6e8897d98bfdf991deda5017dd0e599
Haider Zaman <chhaider10954@gmail.com>
2022-04-20 02:12:35 +0500

    Nav Completed

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import './nav.css'
+import {AiOutlineHome} from "react-icons/ai";
+import {AiOutlineUser} from "react-icons/ai";
+import {BsBookHalf} from "react-icons/bs";
+import {AiOutlineCustomerService} from "react-icons/ai";
+import {AiOutlineMessage} from "react-icons/ai";
 
 const Nav = () => {
 return (
-    <div>Nav</div>
+    <nav>
+        <a href="#home" className="active"><AiOutlineHome/></a>
+        <a href="#about"><AiOutlineUser/></a>
+        <a href="#experience"><BsBookHalf/></a>
+        <a href="#services"><AiOutlineCustomerService/></a>
+        <a href="#contact"><AiOutlineMessage/></a>
+
+    </nav>
+
 )}
 export default Nav
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,6 +3,6 @@ import './portfolio.css'
 
 const Portfolio = () => {
     return (
-    <div>Portfolio</div>
+    <section id='portfolio'>Portfolio</section>
 )}
 export default Portfolio
