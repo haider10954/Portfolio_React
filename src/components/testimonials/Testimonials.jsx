b78a7e49244e25d2ad48a607ba0f669aa0d76794
Haider Zaman <chhaider10954@gmail.com>
2022-04-20 21:50:48 +0500

    Testimonials Complete

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -5,6 +5,14 @@ import AVTR2 from '../../assets/avatar2.jpg'
 import AVTR3 from '../../assets/avatar3.jpg'
 import AVTR4 from '../../assets/avatar4.jpg'
 
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 const data = [
     {
         avatar: AVTR1,
@@ -36,24 +44,29 @@ return (
     <section id='testimonials'>
         <h5>Reviews From Clients</h5>
         <h2>Testimonials</h2>
-        <div className="container testimonials__container">
+        <Swiper className="container testimonials__container"
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        >
             {
                 data.map (({avatar,name,review},index) =>
                 {
                     return(
-                    <article key={index} className="testimonials">
+                    <SwiperSlide key={index} className="testimonials">
                     <div className="client__avatar">
                         <img src={avatar} alt="Avatar one" />
                     </div>
                     <h5 className="client__name">{name}</h5>
                         <small className="client__review">{review}
                         </small>
-                </article>)
+                </SwiperSlide>)
                 }
                 )
             }
 
-        </div>
+        </Swiper>
     </section>
 )
 }
